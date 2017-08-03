11bc1dc5581f4cfc6bbc5d55bdbba7bfdbd77374
Alan Thomson <alan@projectmccoll.co.uk>
2017-08-03 15:54:56 +0100

    Migrate database on deploy

--- api/gulpfile.js
+++ api/gulpfile.js
@@ -12,14 +12,35 @@ gulp.task('default', function (cb) {
     console.log(stderr);
   });
 
-  // install dev dependencies
+
+  // BACKEND
+
+  // install dev dependencies for Backend
+  execSync('npm install --only=dev', {
+    cwd: path.join(__dirname)
+  }, function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+  });
+
+  // run Database migration
+  execSync(path.join(__dirname, 'node_modules', '.bin', 'sequelize') + ' db:migrate', {
+    cwd: path.join(__dirname)
+  }, function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+  });
+
+
+  // FRONTEND
+
+  // install dev dependencies for Frontend
   execSync('npm install --only=dev', {
     cwd: path.join(__dirname, '..', 'my-app')
   }, function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
   });
-
 
   // run NG build process
   execSync(path.join(__dirname, '..', 'my-app', 'node_modules', '.bin', 'ng') + ' build -prod --aot=false', {
